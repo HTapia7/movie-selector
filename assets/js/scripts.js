530543caec59129d7004acde9c373dccc8d594f5
Dionne Angeles <jr8dionne@yahoo.com>
2022-10-27 19:58:13 -0700

    added subscribe and copyright

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,4 +1,14 @@
+
+
 document.addEventListener('DOMContentLoaded', function() {
     var elems = document.querySelectorAll('.sidenav');
-    var instances = M.Sidenav.init(elems, options);
+    var instances = M.Sidenav.init(elems);
   });
+
+  document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.fixed-action-btn');
+    var instances = M.FloatingActionButton.init(elems);
+  });
+
+  
+    }

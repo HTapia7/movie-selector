ff87af56f4c5f213016ff2d30f3c8d74c852b67b
Jeevan Johal <jkjohal@ucdavis.edu>
2022-10-31 15:48:22 -0700

    added watchlist buttons

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,9 +1,12 @@
+ //Modal//
+ 
  document.addEventListener('DOMContentLoaded', function() {
     var elems = document.querySelectorAll('.modal');
     var instances = M.Modal.init(elems);
   });
 
-  var myApiKey = '9b5395eaf05fdecc1777b99cac9a49d7'; 
+//API 1 for cards//
+var myApiKey = '9b5395eaf05fdecc1777b99cac9a49d7'; 
 var baseUrl = 'https://api.themoviedb.org/3/discover/movie?api_key=' + myApiKey + '&language=en-US&sort_by=popularity.desc';
 
 var form = document.querySelector('#search');
@@ -32,19 +35,20 @@ function movieData(){
                 movieCard.classList.add('card')
                 movieCard.innerHTML = `
                 <div class="card-image waves-effect waves-block waves-light">
-                    <img class="activator" src="${pictureUrl + element.poster_path}">
-                    
-                </div>
+                <img class="activator" src="${pictureUrl + element.poster_path}">
                 
-                <div class="card-content">
-                    <span class="card-title activator grey-text text-darken-4">${element.title}<i class="material-icons right">more_vert </i></span>
-                    
-                </div>
-      
-                <div class="card-reveal">
-                    <span class="card-title grey-text text-darken-4">Overview<i class="material-icons right">close</i></span>
-                    <p>${element.overview}</p>
-                 </div>
+            </div>
+            
+            <div class="card-content">
+                <span class="card-title activator grey-text text-darken-4">${element.title}<i class="material-icons right">more_vert </i></span>
+                <p class = "watchlist-btn"><a href="#">Add to Watchlist</a></p>
+
+            </div>
+  
+            <div class="card-reveal">
+                <span class="card-title grey-text text-darken-4">Overview<i class="material-icons right">close</i></span>
+                <p>${element.overview}</p>
+             </div>
                  
                 `
                 container.appendChild(movieCard)
@@ -54,3 +58,27 @@ function movieData(){
 
 // Info I want from database is Title, poster , vote_average , overview 
 movieData()
+
+// //Local Storage (adding movies to watchlist)
+
+// var watchlistBtn = document.querySelector("#watchlist-btn");
+// var watchlist = document.querySelector("#watchlist-items");
+
+
+
+// // function createListItem(){
+// //   const newLi = watchlist.createElement("li")
+  
+// //   const listItemContent = document.newLi(card.title)
+// // };
+
+// //event listener for click
+// watchlistBtn.addEventListener("click", function(){
+//   document.watchlist.appendChild(newLi)
+//   var newLi = document.watchlist.createElement("li")
+//   newLi.textcontent = "this.card-title" 
+// });
+
+// //append to watchlist and add to local storage
+
+// //get items from local storage
--- readme.md
+++ readme.md
@@ -1,4 +1,16 @@
-# Newflix
+# NewFlix
 
 ## Description
 Newflix is a site where you can view today's newest movies, add them to your watchlist, and view today's top box office.
+
+## Usage
+Click the "more" icon (the three dots) to view the description for each film.  Click the "Add to Watchlist" button on each film to add it to your watchlist.
+
+## Credits
+APIs Used:
+*TMDB
+
+Framework used:
+*Materialize
+
+## Contributers
